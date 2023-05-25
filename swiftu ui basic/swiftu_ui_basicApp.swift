b6174d9dcc44456932ee302d54cc1c77c0d04a81
Mukhiddin <sirojiddinovm720@mail.ru>
2023-05-25 09:21:34 +0500

    Initial Commit

---
+++ swiftu ui basic/swiftu_ui_basicApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftu_ui_basicApp.swift
+//  swiftu ui basic
+//
+//  Created by Abbos Bobomurodov on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftu_ui_basicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
